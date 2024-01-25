e954b488b982af77432dda8c16f79fa5e0a33883
Daniel Pigan <pigan.daniel@gmail.com>
2024-01-25 14:32:02 +0100

    Add initial code.

---
+++ js/spreadsheet.js
@@ -0,0 +1,26 @@
+const base = `https://docs.google.com/spreadsheets/d/${spreadsheetID}/gviz/tq?`;
+
+export async function fetchTable(sheetName, query) {
+  const data = [];
+  const queryEncoded = encodeURIComponent(query);
+  const url = `${base}&sheet=${sheetName}&tq=${queryEncoded}`;
+  return fetch(url)
+    .then(res => res.text())
+    .then(rep => {
+      const jsData = JSON.parse(rep.substr(47).slice(0, -2));
+      const colz = [];
+      jsData.table.cols.forEach((heading) => {
+        if (heading.label) {
+          colz.push(heading.label.toLowerCase().replace(/\s/g, ''));
+        }
+      })
+      jsData.table.rows.forEach((main) => {
+        const row = {};
+        colz.forEach((ele, ind) => {
+          row[ele] = (main.c[ind] != null) ? main.c[ind].v : '';
+        })
+        data.push(row);
+      })
+      return data
+    })
+}
